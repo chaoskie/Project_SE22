9a17c34889c178209c4876a5d252ca04e6f3588f
Lars <l.blom@student.fontys.nl>
2016-01-08 15:28:45 +0100

    added unittest passhashing

--- tronpon/tronpon.Tests/UnitTest1.cs
+++ tronpon/tronpon.Tests/UnitTest1.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tronpon_Classes;
 
 namespace tronpon.Tests
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        public void Test_Hashing()
         {
+            string SendThis = "WachtwoordHash";
+            string Hashed = PasswordHash.CreateHash(SendThis);
+            Assert.AreNotEqual(SendThis, Hashed, string.Format("Raw wachtwoord ({0}) is gelijk aan Hashedwachtwoord ({1})", SendThis, Hashed));
+
+            SendThis = string.Empty;
+            Hashed = PasswordHash.CreateHash(SendThis);
+            Assert.AreNotEqual(SendThis, Hashed, string.Format("Raw wachtwoord ({0}) is gelijk aan Hashedwachtwoord ({1})", SendThis, Hashed));
+        }
+
+        [TestMethod]
+        public void Hashing_Uniek()
+        {
+            string SendThis = "WachtwoordHash";
+            string Hashed_A = PasswordHash.CreateHash(SendThis);
+            string Hashed_B = PasswordHash.CreateHash(SendThis);
+
+            Assert.AreNotEqual(Hashed_A, Hashed_B, string.Format("Gehashed wachtenwoorden met identieke start ({0}) zijn gelijk.", SendThis));
+        }
+
+        [TestMethod]
+        public void Login_Hash()
+        {
+            string SendThis = "WachtwoordHash";
+            string RegisterPass = PasswordHash.CreateHash(SendThis);
+            Assert.IsTrue(PasswordHash.ValidatePassword(SendThis, RegisterPass), "Login simulatie was niet succesvol.");
         }
     }
 }
